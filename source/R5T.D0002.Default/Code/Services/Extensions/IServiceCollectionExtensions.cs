07436ebed54e29f2823aa6406c846e6effd2c444
MinexAutomation <minex.automation@gmail.com>
2020-05-15 00:16:08 -0700

    Added R5T.D0002.Standard INowProvider standard aggregation library.

---
+++ source/R5T.D0002.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0002.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="NowProvider"/> implementation of <see cref="INowProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultNowProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<INowProvider, NowProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="NowProvider"/> implementation of <see cref="INowProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<INowProvider> AddDefaultNowProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<INowProvider>.New(() => services.AddDefaultNowProvider());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0002.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.D0002.Default;
+
+
+namespace R5T.D0002.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="INowProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddNowProvider(this IServiceCollection services)
+        {
+            services.AddDefaultNowProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="INowProvider"/> service.
+        /// </summary>
+        public static ServiceAction<INowProvider> AddNowProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<INowProvider>.New(() => services.AddNowProvider());
+            return serviceAction;
+        }
+    }
+}
